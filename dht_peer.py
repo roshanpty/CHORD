cfec55723cf10a554ee834af1a42a52306d1442d
Hax Turn <roshanthomas@live.com>
2017-11-26 08:32:25 -0800

    working root

--- dht_peer.py
+++ dht_peer.py
@@ -1,11 +1,7 @@
 import argparse
 import socket
 import sys
-
 import hashlib
-
-# DHT peer application.
-# Usage: ./dht_peer <-m type> <-p own_port <-h own_hostname> <-r root_port> <-R root_hostname>
 
 parser = argparse.ArgumentParser(usage='./dht_peer <-m type> <-p own_port <-h own_hostname> <-r root_port> <-R root_hostname>',
                                  description='DHT Peer Application',add_help=False)
@@ -13,24 +9,16 @@ parser._add_action(argparse._HelpAction(
     option_strings=['-H', '--help'],
     help='Gives you the help documentation and details about optional arguments'
 ))
-parser.add_argument('-m','--peertype',type=int,help='Specify 1 if the peer is root and 0 otherwise')
-parser.add_argument('-p','--own_port',help='Specify the port for the peer')
+parser.add_argument('-m','--peertype', type = int, help='Specify 1 if the peer is root and 0 otherwise')
+parser.add_argument('-p','--own_port', type = int, help='Specify the port for the peer')
 parser.add_argument('-h','--own_hostname',help='Specify the hostname of the peer')
-parser.add_argument('-r','--root_port',help='Specify the port of the root')
+parser.add_argument('-r','--root_port', type = int, help='Specify the port of the root')
 parser.add_argument('-R','--root_hostname',help='Specify the hostname of the root')
 args = parser.parse_args()
 
-# Default is normal peer of the type is not specified.
+# Default is normal peer if the type is not specified.
 if args.peertype is None:
     args.peertype = 0
-
-# if args.peertype == 0:
-#     print "Starting as normal peer"
-# elif args.peertype == 1:
-#     print "Starting as root"
-# else:
-#     print "Invalid peer type given. Please give 1 for root and 0 otherwise."
-
 
 peertype = args.peertype
 ownport = args.own_port
@@ -38,24 +26,82 @@ ownhost = args.own_hostname
 rootport = args.root_port
 roothost = args.root_hostname
 
-# This c;ass may be useful in handling the ring position effectively. Initialises  as root. 
-class nodeposition(self, nodenum = 0, preaddr = ownhost, preport = ownport, sucaddr = ownhost,sucport = ownport):
-	self.nodenum = nodenum
-	self.preaddr = preaddr
-	self.preport = preport
-	self.sucaddr = sucaddr
-	self.sucport = sucport
-#Initialize root node
-successor = ownhost + ":" + ownportimport argparse
-predecessor = ownhost + ":" + ownport
+class node:
+	def __init__(self,phname,phport,shname,shport):
+		self.pn = phname
+		self.pp = phport
+		self.sn = shname
+		self.sp = shport
+def printchord(nn):
+	print nn.pn,":", nn.pp, "--- me--->", nn.sn, nn.sp
 
-root_socket = (ownhost, ownport)
-print >>sys.stderr, 'starting root node on %s port %s' % root_socket
-sock.bind(root_socket)
-sock.listen(10) # Maximum 10 connections
+# If m = 1, the node initiated is considered as the root node. 
+if peertype == 1:
+	# Initiating root node
+	rootnode = node(ownhost, ownport, ownhost, ownport)
+	printchord(rootnode)
+	# Creating a socket for root node
+	try:
+		nsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	except socket.error, msg:
+		print 'Failed to create socket. Error code: ' + str(msg[0]) + ' , Error message : ' + msg[1]
+		sys.exit();
+	print "Socket created for root node!"
 
-conn, addr = sock.accept()
-print "A Client has connected at ", addr
-conn.send("ROOT HELLO ") # This should pass the node position information to the clients.
-predecessor = addr + ":" + clientport
-conn.close() 
+	#Binding the socket to specified ports
+	try:
+		nsock.bind((ownhost, ownport))
+	except socket.error , msg:
+		print 'Bind failed. Error Code : ' + str(msg[0]) + ' Message ' + msg[1]
+		sys.exit()
+	print 'Socket bind complete'
+
+	# Listening for incoming requests
+	nsock.listen(10)
+	print 'Socket now listening'
+	conn, addr = nsock.accept()
+	print 'Connected with ' + addr[0] + ':' + str(addr[1])
+
+	# Handling the incoming requests received
+	req = conn.recv(1024)
+	reqpro = req.split('|')
+	# If the request is a join request
+	if reqpro[0] == "JOIN":
+		# 1. Update the predecessor of the current successor
+		try:
+			sock2 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		except socket.error, msg:
+    			print 'Failed to create socket. Error code: ' + str(msg[0]) + ' , Error message : ' + msg[1]
+    			sys.exit();
+
+		print '\________________________\nSocket Created for updating the predecessor of current successor\n'
+		try:
+			remote_ip = socket.gethostbyname( rootnode.sn )
+		except socket.gaierror:
+    			#could not resolve
+    			print 'Hostname could not be resolved. Exiting'
+    			sys.exit()
+		print 'Ip address of current successor %s is %s' %(rootnode.sn, remote_ip)
+		#Connect to current successor of root
+		sock2.connect((remote_ip , rootnode.sp))
+		print 'Socket Connected to %s on port %d'  %(rootnode.sn, rootnode.sp)
+		#Sending the UPDATE request"
+		predupdata = "UPDATE|PRED|"+ reqpro[1]+"|"+reqpro[2]
+		try :
+		#Set the whole string
+			sock2.sendall(predupdata)
+		except socket.error:
+    		#Send failed
+			print 'Send failed'
+			sys.exit()
+		print 'Message send successfully \n %s to %s on %d'  %(predupdata,rootnode.sn,rootnode.sp)
+		# 2. Update the successor of the joining node
+		sucupdate = "UPDATE|SUCC|"+rootnode.sn+"|"+str(rootnode.sp)
+		conn.sendall(sucupdate)
+		# 3. Update the successor of the root node 
+		rootnode.sn = reqpro[1]
+		rootnode.sp = reqpro[2]
+
+		printchord(rootnode)
+		conn.close()
+
